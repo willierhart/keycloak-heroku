61b4146a905005d1baeff0a280c0c0bf01c2305b
Jeff Tian <jeff.tian@outlook.com>
2024-02-15 21:59:59 +0800

    fix(https://github.com/Jeff-Tian/keycloak-phone-provider/issues/2): fix the 404 endpoint issue

---
+++ src/main/java/net/brickverse/server/KeycloakServer.java
@@ -0,0 +1,9 @@
+package net.brickverse.server;
+
+import org.keycloak.services.resources.KeycloakApplication;
+
+public class KeycloakServer {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
